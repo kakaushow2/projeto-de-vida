67066f4bfef1f06766731c52858bfb7dc99e021c
kakaushow2 <kauane.fernandes.cardoso@escola.pr.gov.br>
2026-04-30 11:15:27 +0000

    4 atividade

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 <script src="main.js"></script>
 const botoes = document.querySelectorAll(".botao");
 console.log(botoes);
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
+      textos[j].classList.remove("ativo");
+    }
+
+    botoes[i].classList.add("ativo");
+    textos[i].classList.add("ativo");
+  };
+}
