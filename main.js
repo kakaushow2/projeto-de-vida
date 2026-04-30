6405985d577fba148396ce75bb324fea2ec29d3d
kakaushow2 <kauane.fernandes.cardoso@escola.pr.gov.br>
2026-04-30 11:44:45 +0000

    8 atividade

--- main.js
+++ main.js
@@ -76,3 +76,34 @@ if (tempoFinal > 0) {
 } else {
   return "Prazo Finalizado";
 }
+if (tempoFinal > 0) {
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+} else {
+  return [0, 0, 0, 0];
+}
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+
+  segundos %= 60;
+  minutos %= 60;
+  horas %= 24;
+  if (tempoFinal > 0) {
+    return [dias, horas, minutos, segundos];
+  } else {
+    return [0, 0, 0, 0];
+  }
+}
