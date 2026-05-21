f2fb5d57f62ec23e36998641dea50ca9881976cf
kakaushow2 <kauane.fernandes.cardoso@escola.pr.gov.br>
2026-05-21 11:36:52 +0000

    aula 3

--- main.js
+++ main.js
@@ -1,18 +1,11 @@
-const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
-const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
+const botoes = document.querySelectorAll(".botao");
 
-function atualizaCronometro() {
-  for (let i = 0; i < contadores.length; i++) {
-    document.getElementById("dias" + i).textContent = calculaTempo(tempos[i])[0];
-        document.getElementById("horas" + i).textContent = calculaTempo(tempos[i])[1];
-        document.getElementById("min" + i).textContent = calculaTempo(tempos[i])[2];
-        document.getElementById("seg" + i).textContent = calculaTempo(tempos[i])[3];
+for (let i = 0; i < botoes.length; i++) {
+  botoes[i].onclick = function () {
+    for (let j = 0; j < botoes.length; j++) {
+      botoes[j].classList.remove("ativo");
     }
-}
 
-function comecaCronometro() {
-    atualizaCronometro();
-    setInterval(atualizaCronometro, 1000);
+    botoes[i].classList.add("ativo");
+  };
 }
