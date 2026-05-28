97b3b3128f8bf44dcfab9ac464cf725cde7787d7
kakaushow2 <kauane.fernandes.cardoso@escola.pr.gov.br>
2026-05-28 11:37:40 +0000

    aula 4

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
-
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
     for (let j = 0; j < botoes.length; j++) {
@@ -10,6 +9,10 @@ for (let i = 0; i < botoes.length; i++) {
 
     botoes[i].classList.add("ativo");
     textos[i].classList.add("ativo");
-  }
+  };
+}
 const contadores = document.querySelectorAll(".contador");
-contadores[0].textContent = "Contagem regressiva";
+const tempoObjetivo1 = new Date("2026-05-28T08:37:00");
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
