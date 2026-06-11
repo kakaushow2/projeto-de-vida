34e26f6c12e0042b2cc2ad6f1112f1ad1f30c4e1
kakaushow2 <kauane.fernandes.cardoso@escola.pr.gov.br>
2026-06-11 11:33:18 +0000

    aula 6

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
+
 for (let i = 0; i < botoes.length; i++) {
   botoes[i].onclick = function () {
     for (let j = 0; j < botoes.length; j++) {
@@ -11,8 +12,42 @@ for (let i = 0; i < botoes.length; i++) {
     textos[i].classList.add("ativo");
   };
 }
-const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2026-05-28T08:37:00");
-let tempoAtual = new Date();
 
-contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2026-12-12T00:00:00");
+const tempoObjetivo2 = new Date("2026-10-02T00:00:00");
+const tempoObjetivo3 = new Date("2027-02-09T00:00:00");
+const tempoObjetivo4 = new Date("2026-12-31T00:00:00");
+
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function calculaTempo(tempoObjetivo) {
+  let tempoAtual = new Date();
+  let tempoFinal = tempoObjetivo - tempoAtual;
+  let segundos = Math.floor(tempoFinal / 1000);
+  let minutos = Math.floor(segundos / 60);
+  let horas = Math.floor(minutos / 60);
+  let dias = Math.floor(horas / 24);
+  return (
+    dias +
+    " dias " +
+    horas +
+    " horas " +
+    minutos +
+    " minutos " +
+    segundos +
+    " segundos"
+  );
+}
+function atualizaCronometro() {
+  for (let i = 0; i < contadores.length; i++) {
+    contadores[i].textContent = calculaTempo(tempos[i]);
+  }
+}
+
+function comecaCronometro() {
+  atualizaCronometro();
+  setInterval(atualizaCronometro, 1000);
+}
+
+comecaCronometro();
